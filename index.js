3c9f1994e50425c48132582b7cf77ea1df829056
arupp <arup@pitangent.com>
2024-09-13 08:29:51 +0530

    Initialize project

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello World</h1>");
+})
+
+server.listen(3000, () => console.log('Server is running on port 3000'));
